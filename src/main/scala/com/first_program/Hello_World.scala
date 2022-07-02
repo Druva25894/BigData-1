d6cf8865fcf107855476f26e84860055766ca193
krishna_windell <gkcomputers21@gmail.com>
2022-07-02 13:02:34 +0530

    first commit

---
+++ src/main/scala/com/first_program/Hello_World.scala
@@ -0,0 +1,18 @@
+package com.first_program
+
+import org.apache.spark.sql.SparkSession
+
+object Hello_World {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .appName("first_app")
+      .master("local")
+      .getOrCreate()
+
+    println("helloworld")
+
+
+
+  }
+
+}
