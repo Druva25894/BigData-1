1abb29de5a073a5c2727c9f29d9729d61fe38689
krishna_windell <gkcomputers21@gmail.com>
2022-07-05 15:49:57 +0530

    control statements

---
+++ src/main/scala/Control_Statements/Loops.scala
@@ -0,0 +1,45 @@
+package Control_Statements
+
+object Loops extends App {
+
+  var x = 1;
+
+  // Exit when x becomes greater than 4
+  while (x <= 4) {
+    println("Value of x: " + x);
+
+    // Increment the value of x for
+    // next iteration
+    x = x + 1;
+  }
+
+
+}
+
+object while_loop {
+  def main(args: Array[String]): Unit = {
+    println("\n---------- While loop itteration -----------")
+    var z = 9
+    while (z < 11) {
+      //println(z);
+      z = z + 1;
+    }
+
+
+    var f=20
+    while(f <= 25)
+      {
+        //println(f)
+        f = f + 1
+      }
+
+
+    while(9 <= 0 )
+    {
+      println(c)
+      c = c - 1
+    }
+
+  }
+}
+
